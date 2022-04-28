9a6f6602f32f342059b4762073f0ba54d8bb7db8
Lucas Coutinho <lucas.coutinho@eagronom.com>
2022-04-28 17:36:43 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -17,20 +17,19 @@ export default function Home() {
          <div className="profile__data">
            <div className="profile__border">
              <div className="profile__perfil">
-               <Image src={lucas}></Image>
+               <Image src={lucas} alt=''></Image>
              </div>
            </div>
             <h2 className="profile__name"> Lucas Coutinho </h2>
             <h3 className="profile__profession"> BlockChain and Web Developer </h3>
-
             <ul className="profile__social">
-              <a href="https://www.instagram.com/lucas.dcoutinho/" target='_blank' className="profile__social-link">
+              <a href="https://www.instagram.com/lucas.dcoutinho/" className="profile__social-link">
                 <InstagramIcon/>
               </a>
-              <a href="https://www.linkedin.com/in/lucasdcoutinho/" target="_blank" className="profile__social-link">
+              <a href="https://www.linkedin.com/in/lucasdcoutinho/" className="profile__social-link">
                 <LinkedInIcon/>
               </a>
-              <a href="https://github.com/lucasdcoutinh0" target="_blank" className="profile__social-link">
+              <a href="https://github.com/lucasdcoutinh0" className="profile__social-link">
                 <GitHubIcon/>
               </a>
             </ul>
@@ -56,21 +55,16 @@ export default function Home() {
 
                 <div className="profile__buttons">
                 <a href="https://drive.google.com/file/d/1EKmXJjLgggzkaX6Wbzofo65zPPKeaS_O/view?usp=sharing" className='button'>Download CV <DownloadIcon/> </a>
-                    <div class="profile__buttons-small">
-                        <a href="https://api.whatsapp.com/send?phone=5511977194234&text=Hey,%20I%20want%20to%20talk%20with%20you%20abou%20a%20developer%20service%20" target="_blank" className="button button__small button__gray">
+                    <div className="profile__buttons-small">
+                        <a href="https://api.whatsapp.com/send?phone=5511977194234&text=Hey,%20I%20want%20to%20talk%20with%20you%20abou%20a%20developer%20service%20" className="button button__small button__gray">
                             <WhatsAppIcon/>
                         </a>
-                        <a href="https://tttttt.me/lucasdcoutinho" target="_blank" className="button button__small button__gray">
+                        <a href="https://tttttt.me/lucasdcoutinho" className="button button__small button__gray">
                             <TelegramIcon/>
                         </a>
                     </div>
                 </div>
                   <Pages/>
-        <footer className="footer container">
-            <span className="footer__copy">
-                &#169; Coutinho's Solutions. All rigths reserved
-            </span>
-        </footer>
             </div>
      </header>
    </div>
